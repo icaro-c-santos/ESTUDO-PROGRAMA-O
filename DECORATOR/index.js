401649fffdf3b62068d4b43ff4dccfce2420cbd7
ICARO SANTOS <icarosalna@hotmail.com>
2023-03-04 17:57:51 -0300

    understand decorator

--- DECORATOR/index.js
+++ DECORATOR/index.js
@@ -4,34 +4,35 @@
 
  */
 
-const decoratr = (fn) => {
-  return (...params) => {
-    console.log("antes da função");
-    const resultado = fn.call(this, ...params);
-    console.log("depois da função");
-    return resultado;
-  };
-};
-
-const decorator = (fn) => {
-  return function (...params) {
-    const resultado = fn.call(this, ...params);
-    return JSON.stringify(resultado);
-  };
-};
-
-const decoratorLog = (fn, messageLog) => {
-  return function (...params) {
-    const resultado = fn.call(this, ...params);
-    console.log("LOG ->", messageLog);
-    return resultado;
-  };
-};
-
-function apiEndpoint(requestValue) {
-  return { name: "FULADO DE TAL SILVA" };
-}
-
-const endpointDecorator = decorator(apiEndpoint);
-const logDecorator = decoratorLog(endpointDecorator,"ENDPOINT DE BUSCA CHAMADO!");
-console.log(logDecorator());
+    const decoratr = (fn) => {
+        return (...params) => {
+          console.log("antes da função");
+          const resultado = fn.call(this, ...params);
+          console.log("depois da função");
+          return resultado;
+        };
+      };
+      
+      const decorator = (fn) => {
+        return function (...params) {
+          const resultado = fn.call(this, ...params);
+          return JSON.stringify(resultado);
+        };
+      };
+      
+      const decoratorLog = (fn, messageLog) => {
+        return function (...params) {
+          const resultado = fn.call(this, ...params);
+          console.log("LOG ->", messageLog);
+          return resultado;
+        };
+      };
+      
+      function apiEndpoint(requestValue) {
+        return { name: "FULADO DE TAL SILVA" };
+      }
+      
+      const endpointDecorator = decorator(apiEndpoint);
+      const logDecorator = decoratorLog(endpointDecorator,"ENDPOINT DE BUSCA CHAMADO!");
+      console.log(logDecorator());
+      
