64677ae2dd39136fa6365f5e0022cac91b1aa1df
ICARO SANTOS <icaro.santos@sanar.com>
2023-03-04 17:56:34 -0300

    add Decorators

---
+++ DECORATOR/index.js
@@ -0,0 +1,37 @@
+/* 
+
+    Eles adicionam novos comportamentos a classes, funções ou metodos.
+
+ */
+
+const decoratr = (fn) => {
+  return (...params) => {
+    console.log("antes da função");
+    const resultado = fn.call(this, ...params);
+    console.log("depois da função");
+    return resultado;
+  };
+};
+
+const decorator = (fn) => {
+  return function (...params) {
+    const resultado = fn.call(this, ...params);
+    return JSON.stringify(resultado);
+  };
+};
+
+const decoratorLog = (fn, messageLog) => {
+  return function (...params) {
+    const resultado = fn.call(this, ...params);
+    console.log("LOG ->", messageLog);
+    return resultado;
+  };
+};
+
+function apiEndpoint(requestValue) {
+  return { name: "FULADO DE TAL SILVA" };
+}
+
+const endpointDecorator = decorator(apiEndpoint);
+const logDecorator = decoratorLog(endpointDecorator,"ENDPOINT DE BUSCA CHAMADO!");
+console.log(logDecorator());
